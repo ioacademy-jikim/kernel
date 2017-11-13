04c7248d09ffa4f282fa6898bfebe00930613291
Your Name <you@example.com>
2017-11-13 13:17:45 +0900

    second commit

---
+++ 01_day/module_2.c
@@ -0,0 +1,19 @@
+#include <linux/module.h> // init_module, cleanup_module
+#include <linux/kernel.h> // printk
+#include <linux/gpio.h>   // gpio_get_value
+
+int init_module(void)
+{
+	int ret;
+	printk("init_module\n");
+	ret = __gpio_get_value( 170 );
+	printk("ret=%d\n", ret );
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk("cleanup_module\n");
+}
+
+MODULE_LICENSE("GPL");
